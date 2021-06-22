04f721d4cc8ca13612c7a869e7b987dfc1ed63ca
ddsjoberg <danield.sjoberg@gmail.com>
2021-06-22 12:31:34 -0400

    Update test-create_project.R

--- tests/testthat/test-create_project.R
+++ tests/testthat/test-create_project.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+proj_dir <- fs::path(tempdir(), "My Project Folder")
+
+test_that("create_project() works", {
+  expect_error(
+    create_project(
+      path = proj_dir,
+      open = FALSE # don't open project in new RStudio session
+    ),
+    NA
+  )
 })
