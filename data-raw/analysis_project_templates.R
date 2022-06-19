8b217a0667d01cdf2431eab9049a2c281886c1ed
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-06-19 12:18:06 -0400

    adding templates

---
+++ data-raw/analysis_project_templates.R
@@ -0,0 +1,63 @@
+analysis_project_template <-
+  list(
+    readme = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/default_readme.md", package = "starter"),
+      filename = "README.md",
+      copy = FALSE
+    )),
+    gitignore = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/default_gitignore.txt", package = "starter"),
+      filename = ".gitignore",
+      copy = TRUE
+    )),
+    rproj = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/default_rproj.Rproj", package = "starter"),
+      filename = glue::glue("_rstudio_project.Rproj"),
+      copy = TRUE
+    )),
+    setup = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/analysis_template/setup.Rmd", package = "starter"),
+      filename = glue::glue("scripts/10-setup_{folder_first_word}.Rmd"),
+      copy = FALSE
+    )),
+    analysis = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/analysis_template/analysis.Rmd", package = "starter"),
+      filename = glue::glue("scripts/20-analysis_{folder_first_word}.Rmd"),
+      copy = FALSE
+    )),
+    report = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/analysis_template/report.Rmd", package = "starter"),
+      filename = glue::glue("scripts/30-report_{folder_first_word}.Rmd"),
+      copy = FALSE
+    )),
+    doc_template = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/analysis_template/doc_template.docx", package = "starter"),
+      filename = "scripts/templates/doc_template.docx",
+      copy = TRUE
+    )),
+    references = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/analysis_template/references.bib", package = "starter"),
+      filename = glue::glue("scripts/templates/references.bib"),
+      copy = TRUE
+    )),
+    derived_vars = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/analysis_template/derived_variables.xlsx", package = "starter"),
+      filename = glue::glue("scripts/derived_variables.xlsx"),
+      copy = TRUE
+    )),
+    sap = rlang::expr(list(
+      template_filename = fs::path_package("project_templates/analysis_template/sap.docx", package = "starter"),
+      filename = glue::glue("SAP - {folder_name}.docx"),
+      copy = TRUE
+    )),
+    # only add Rprofile if renv was used
+    rprofile =
+      rlang::expr(switch(renv,
+                         list(
+                           template_filename =
+                             fs::path_package(package = "starter", "project_templates/default_rprofile.R"),
+                           filename = stringr::str_glue(".Rprofile"),
+                           glue = TRUE
+                         )
+      ))
+  )
--- data-raw/project_templates.R
+++ data-raw/project_templates.R
@@ -1,29 +1,29 @@
 ## code to prepare project template goes here
 
-# default template
+# default template -------------------------------------------------------------
 default_project_template <-
-  quote(list(
-    readme = list(
+  list(
+    readme = rlang::expr(list(
       template_filename =
         fs::path_package(package = "starter", "project_templates/default_readme.md"),
       filename = "README.md",
       glue = TRUE
-    ),
-    gitignore = list(
+    )),
+    gitignore = rlang::expr(list(
       template_filename =
         fs::path_package(package = "starter", "project_templates/default_gitignore.txt"),
       filename = ".gitignore",
       glue = FALSE
-    ),
-    rproj = list(
+    )),
+    rproj = rlang::expr(list(
       template_filename =
         fs::path_package(package = "starter", "project_templates/default_rproj.Rproj"),
       filename = stringr::str_glue("{folder_name}.Rproj"),
       glue = FALSE
-    ),
+    )),
     # only add Rprofile if renv was used
     rprofile =
-      switch(
+      rlang::expr(switch(
         renv,
         list(
           template_filename =
@@ -32,11 +32,59 @@ default_project_template <-
           filename = stringr::str_glue(".Rprofile"),
           glue = TRUE
         )
-      )
-  ))
+      ))
+  )
 attr(default_project_template, "label") <- "Default Project Template"
+
+
+# analysis template ------------------------------------------------------------
+analysis_project_template <-
+  c(default_project_template,
+    list(
+      setup = rlang::expr(list(
+        template_filename = fs::path_package("project_templates/analysis_template/setup.Rmd", package = "starter"),
+        filename = glue::glue("scripts/10-setup_{folder_first_word}.Rmd"),
+        copy = FALSE
+      )),
+      analysis = rlang::expr(list(
+        template_filename = fs::path_package("project_templates/analysis_template/analysis.Rmd", package = "starter"),
+        filename = glue::glue("scripts/20-analysis_{folder_first_word}.Rmd"),
+        copy = FALSE
+      )),
+      report = rlang::expr(list(
+        template_filename = fs::path_package("project_templates/analysis_template/report.Rmd", package = "starter"),
+        filename = glue::glue("scripts/30-report_{folder_first_word}.Rmd"),
+        copy = FALSE
+      )),
+      doc_template = rlang::expr(list(
+        template_filename = fs::path_package("project_templates/analysis_template/doc_template.docx", package = "starter"),
+        filename = "scripts/templates/doc_template.docx",
+        copy = TRUE
+      )),
+      references = rlang::expr(list(
+        template_filename = fs::path_package("project_templates/analysis_template/references.bib", package = "starter"),
+        filename = glue::glue("scripts/templates/references.bib"),
+        copy = TRUE
+      )),
+      derived_vars = rlang::expr(list(
+        template_filename = fs::path_package("project_templates/analysis_template/derived_variables.xlsx", package = "starter"),
+        filename = glue::glue("scripts/derived_variables.xlsx"),
+        copy = TRUE
+      )),
+      sap = rlang::expr(list(
+        template_filename = fs::path_package("project_templates/analysis_template/sap.docx", package = "starter"),
+        filename = glue::glue("SAP - {folder_name}.docx"),
+        copy = TRUE
+      ))
+    )
+  )
+attr(analysis_project_template, "label") <- "Analysis Project Template"
+
+
+
 
 # save templates ---------------------------------------------------------------
 project_templates <- list()
 project_templates[["default"]] <- default_project_template
-usethis::use_data(project_templates, overwrite = TRUE, internal = TRUE)
+project_templates[["analysis"]] <- analysis_project_template
+usethis::use_data(project_templates, overwrite = TRUE, internal = FALSE)
