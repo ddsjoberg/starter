8a609d31be3cae5f65cab3c945538a8eea7ea1ff
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-06-18 21:03:25 -0400

    updated messaging in default rprofile file

--- inst/project_templates/default_rprofile.R
+++ inst/project_templates/default_rprofile.R
@@ -1,7 +1,7 @@
 
 # if renv project & it's bare, print message to record pkgs in R and Rmd scripts
 if (file.exists("renv.lock") &&
-    length(setdiff(installed.packages(.libPaths()[1])[, 1], "renv")) == 0L) {
+    length(setdiff(names(renv:::renv_lockfile_read(file = "renv.lock")$Packages), "renv")) == 0L) {
   # if cli available, print a pretty message to finish setting up renv
   if ("cli" %in% installed.packages()[, 1]) {
     cli::cli_alert_danger("Your {.pkg renv} project is {.strong not} yet setup.")
